6eb4d372f0ebaff94fa2fa64962f1426085fe5a6
dsseo <dev.jsds@gmail.com>
2022-03-16 22:57:19 +0900

    Initial commit

---
+++ lib/dev_jsds_logo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:logo_sample/dev_jsds_logo_painter.dart';
+
+class DevJSDSLogo extends StatelessWidget {
+  const DevJSDSLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      height: 300,
+      child: CustomPaint(
+        painter: DevJSDSLogoPainter(),
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:logo_sample/clock_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: const Color(0xFF2D2F41),
+        child: ClockView(),
+      ),
+    );
+  }
+}
